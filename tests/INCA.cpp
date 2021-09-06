b7025e982ebc77f5ef081f3b62c589c51c4f8937
NickTacke <the.nick.tacke@gmail.com>
2021-09-06 11:30:00 +0000

    tests: vectors for INCA

--- tests/INCA.cpp
+++ tests/INCA.cpp
@@ -1,5 +1,6 @@
 // regression vectors for INCA (0x18)
-static void run() {
-    // expected: correct A/X/flags after INCA
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute INCA, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

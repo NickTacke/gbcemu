e5fc0d9f1790cdcf5bb569553997c4bacd0314a7
NickTacke <the.nick.tacke@gmail.com>
2021-09-08 20:15:00 +0000

    tests: vectors for ADDA

--- tests/ADDA.cpp
+++ tests/ADDA.cpp
@@ -1,5 +1,6 @@
 // regression vectors for ADDA (0x08)
-static void run() {
-    // expected: correct A/X/flags after ADDA
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute ADDA, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

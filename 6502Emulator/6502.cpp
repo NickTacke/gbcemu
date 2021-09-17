d2f884d9d6ff121e8347c77d58d0ae4faa26c8bf
NickTacke <the.nick.tacke@gmail.com>
2021-09-17 20:00:00 +0000

    cpu: normalize operand-size handling for immediate forms

--- 6502Emulator/6502.cpp
+++ 6502Emulator/6502.cpp
@@ -139,3 +139,4 @@ u32 CPU::Execute(u32 Cycles, Memory& memory)
 	return (CyclesCopy - Cycles);
 }
 // cpu: clarify branch-target resolution
+// cpu: normalize operand-size handling for immediate forms

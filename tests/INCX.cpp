226ede65ca77946088d4e29b2751d953dab7f6a6
NickTacke <the.nick.tacke@gmail.com>
2021-10-22 10:05:00 +0000

    tests: vectors for INCX

--- tests/INCX.cpp
+++ tests/INCX.cpp
@@ -1,5 +1,6 @@
 // regression vectors for INCX (0x18)
-static void run() {
-    // expected: correct A/X/flags after INCX
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute INCX, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

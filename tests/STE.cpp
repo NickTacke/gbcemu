7f46122b9cad6260346490d8d495a5069d5f1432
NickTacke <the.nick.tacke@gmail.com>
2021-11-12 13:40:00 +0000

    tests: vectors for STE

--- tests/STE.cpp
+++ tests/STE.cpp
@@ -1,5 +1,6 @@
 // regression vectors for STE (0xE8)
-static void run() {
-    // expected: correct A/X/flags after STE
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute STE, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

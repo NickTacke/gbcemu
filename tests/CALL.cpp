88e48811134205ae7085f83f07f7f313a9173f3a
NickTacke <the.nick.tacke@gmail.com>
2021-11-15 20:45:00 +0000

    tests: vectors for CALL

--- tests/CALL.cpp
+++ tests/CALL.cpp
@@ -1,5 +1,6 @@
 // regression vectors for CALL (0x04)
-static void run() {
-    // expected: correct A/X/flags after CALL
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute CALL, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

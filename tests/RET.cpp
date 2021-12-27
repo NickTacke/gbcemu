cdc83e8066233b1d4ceb4096c9c935de2a25ba9a
NickTacke <the.nick.tacke@gmail.com>
2021-12-27 21:15:00 +0000

    tests: vectors for RET

--- tests/RET.cpp
+++ tests/RET.cpp
@@ -1,5 +1,6 @@
 // regression vectors for RET (0x0C)
-static void run() {
-    // expected: correct A/X/flags after RET
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute RET, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

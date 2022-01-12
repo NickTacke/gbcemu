3fb9b8fadd7b09634fa3d63e0cebf4fac7f65cf1
NickTacke <the.nick.tacke@gmail.com>
2022-01-12 22:50:00 +0000

    tests: vectors for RETN

--- tests/RETN.cpp
+++ tests/RETN.cpp
@@ -1,5 +1,6 @@
 // regression vectors for RETN (0xD0)
-static void run() {
-    // expected: correct A/X/flags after RETN
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute RETN, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

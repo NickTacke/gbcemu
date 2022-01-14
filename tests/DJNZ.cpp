d7474b8a05cd068dbf0b4bf3960e4bb57a32983a
NickTacke <the.nick.tacke@gmail.com>
2022-01-14 16:30:00 +0000

    tests: vectors for DJNZ

--- tests/DJNZ.cpp
+++ tests/DJNZ.cpp
@@ -1,5 +1,6 @@
 // regression vectors for DJNZ (0x10)
-static void run() {
-    // expected: correct A/X/flags after DJNZ
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute DJNZ, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

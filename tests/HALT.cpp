025f45cbf82eb9695f82ef2d9834ad9ab38c4a82
NickTacke <the.nick.tacke@gmail.com>
2022-01-28 15:20:00 +0000

    tests: vectors for HALT

--- tests/HALT.cpp
+++ tests/HALT.cpp
@@ -1,5 +1,6 @@
 // regression vectors for HALT (0x77)
-static void run() {
-    // expected: correct A/X/flags after HALT
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute HALT, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

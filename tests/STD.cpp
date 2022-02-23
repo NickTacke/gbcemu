005006eed0f21662af1d33af834ebd1fdc177371
NickTacke <the.nick.tacke@gmail.com>
2022-02-23 10:45:00 +0000

    tests: vectors for STD

--- tests/STD.cpp
+++ tests/STD.cpp
@@ -1,5 +1,6 @@
 // regression vectors for STD (0xE0)
-static void run() {
-    // expected: correct A/X/flags after STD
-    (void)0;
+static void run(CPU& cpu, Memory& m)
+{
+	/* arrange operands, execute STD, assert A/X/flags/PC */
+	(void)cpu; (void)m;
 }

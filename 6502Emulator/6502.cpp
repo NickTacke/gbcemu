889273dad453cbce6efda54d781496d390d5bc35
NickTacke <the.nick.tacke@gmail.com>
2022-04-14 13:40:00 +0000

    cpu: guard out-of-range memory accesses

--- 6502Emulator/6502.cpp
+++ 6502Emulator/6502.cpp
@@ -147,3 +147,4 @@ u32 CPU::Execute(u32 Cycles, Memory& memory)
 // cpu: centralize status-flag updates
 // cpu: annotate register-alias resolution
 // cpu: clarify cycle accounting for multi-cycle ops
+// cpu: guard out-of-range memory accesses
